280f73fe8ce6308a54604e1725f158a86673899d
EmjayAhn <emjay.data@gmail.com>
2018-11-29 21:34:24 +0900

    Feat: practice jQuery

--- practice/routes.py
+++ practice/routes.py
@@ -9,4 +9,18 @@ def home():
 def link_1():
     return render_template('link1.html')
 
+@app.route("/user/<username>")
+def user(username):
+    return render_template('user.html', name=username)
+
+@app.route("/people")
+def people():
+    people = {
+    "user_name1": 20,
+    "user_name2": 30
+    }
+    return jsonify(people)
+
+
+
 app.run(debug=True)
